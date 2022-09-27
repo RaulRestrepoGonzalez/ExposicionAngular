45a314551d66f1a5aa5b6a594881ebf234e02328
RaulRestrepoGonzalez <ralfonsorestrepo@unicesar.edu.co>
2022-09-26 22:59:31 -0500

    proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  image: string;
+  name: string;
+  symbol: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ExposicionAngular';
+
+  api: string =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+  coins: Coin[] = [];
+  titles: string[] = ['#', 'Coin', 'Price', 'Price Change', '24H Volume'];
+  searchText: string = '';
+  filteredCoints: Coin[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Coin[]>(this.api).subscribe(
+      (res) => {
+        this.coins = res;
+        this.filteredCoints = this.coins;
+      },
+      (err) => console.error(err)
+    );
+  }
+
+  searchCoin() {
+    this.filteredCoints = this.coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+
 }
